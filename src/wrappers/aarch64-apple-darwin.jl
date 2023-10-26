1ea77588886d524735009b27da15a13f902b7dcc
jlbuild <juliabuildbot@gmail.com>
2023-10-26 22:53:58 +0000

    utf8proc_jll build 2.9.0+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -2,12 +2,12 @@
 export libutf8proc
 
 JLLWrappers.@generate_wrapper_header("utf8proc")
-JLLWrappers.@declare_library_product(libutf8proc, "@rpath/libutf8proc.2.dylib")
+JLLWrappers.@declare_library_product(libutf8proc, "@rpath/libutf8proc.3.dylib")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libutf8proc,
-        "lib/libutf8proc.2.dylib",
+        "lib/libutf8proc.3.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -2,7 +2,7 @@
 export libutf8proc
 
 JLLWrappers.@generate_wrapper_header("utf8proc")
-JLLWrappers.@declare_library_product(libutf8proc, "libutf8proc.so.2")
+JLLWrappers.@declare_library_product(libutf8proc, "libutf8proc.so.3")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
